3eb172b23ef163af934bfec6796ea98f41bdaa22
mimunojun <mimura.tsukuba@gmail.com>
2023-10-08 09:36:30 +0900

    next init

---
+++ next-app/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <div>(c)2023 すずめのめ All rights reserved.</div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Footer;
